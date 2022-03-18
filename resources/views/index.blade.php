aa8fdc38792b9c7e2aa8a6e5e60fb634ed84f4e1
Juan Esteban <juanes0202@gmail.com>
2022-03-18 15:49:55 -0500

    creando latout personalizado

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.web')
+
+@section('content')
+<div class="text-center">
+    <h1 class="text-3xl text-gray-700 mb-2 uppercase">Últimos cursos</h1>
+    <h2 class="text-xl text-gray-600">Formate online como profesional en tecnología</h2>
+    <h3 class="text-lg text-gray-500">70% de los graduados duplican sus ingresos</h3>
+</div>
+@endsection
