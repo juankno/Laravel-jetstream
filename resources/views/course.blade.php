b3248acf18005caac410341012d4b6db370a2d56
Juan Esteban <juanes0202@gmail.com>
2022-03-19 11:28:29 -0500

    agregando cursos similares al detalle del curso

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -31,6 +31,22 @@
                 <p class="text-gray-400 text-xs">{{ $course->created_at->diffForHumans() }}</p>
             </div>
         </div>
+
+        <div class="grid grid-cols-2 gap-4 my-8">
+            @foreach ($course->similar() as $course)
+            <div class="bg-white shadow-lg rounded-ls px-4 py-6 text-center">
+                <a href="{{ route('course', $course) }}">
+                    <img src="{{ $course->image }}" alt="{{ $course->name }}" class="rounded-md mb-2">
+
+                    <h1 class="text-lg text-gray-600  truncate uppercase">{{ $course->name }}</h1>
+                    <h3 class="text-md text-gray-500 text-left">{{ $course->excerpt }}</h3>
+
+                    <img src="{{ $course->user->avatar }}" alt="{{ $course->user->name }}" class="rounded-full mt-4 mx-auto h-16 w-16">
+                </a>
+            </div>
+            @endforeach
+        </div>
+
     </div>
 </div>
 
