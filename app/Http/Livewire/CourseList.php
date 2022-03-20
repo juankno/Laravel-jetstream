01302a3e80efbbd4a382e7575229d8d4cd1a39f2
Juan Esteban <juanes0202@gmail.com>
2022-03-19 20:10:55 -0500

    agregando paginacion con livewire

--- app/Http/Livewire/CourseList.php
+++ app/Http/Livewire/CourseList.php
@@ -4,13 +4,16 @@ namespace App\Http\Livewire;
 
 use App\Models\Course;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class CourseList extends Component
 {
+    use WithPagination;
+
     public function render()
     {
         return view('livewire.course-list', [
-            'courses' => Course::latest()->with('user')->take(9)->get()
+            'courses' => Course::latest()->with('user')->paginate(9)
         ]);
     }
 }
--- resources/views/livewire/course-list.blade.php
+++ resources/views/livewire/course-list.blade.php
@@ -1,5 +1,11 @@
-<div class="grid grid-cols-3 gap-4 mt-8">
-    @foreach ($courses as $course)
-    <x-course-card :course="$course" />
-    @endforeach
+<div>
+    <div class="grid grid-cols-3 gap-4 mt-8">
+        @foreach ($courses as $course)
+        <x-course-card :course="$course" />
+        @endforeach
+    </div>
+
+    <div class="bg-white px-4 py-3   justify-between border-t border-gray-200 sm:px-6">
+        {{ $courses->links() }}
+    </div>
 </div>
